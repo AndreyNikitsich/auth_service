4fab6e1ff3e361825df90d13b576dde6c1206af1
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-03-04 21:09:34 +0300

    fix waiters for tests

--- tests/functional/wait_for_postgres.py
+++ tests/functional/wait_for_postgres.py
@@ -7,8 +7,7 @@ from settings import test_settings
 if __name__ == "__main__":
     while True:
         try:
-            with psycopg2.connect(test_settings) as conn:
+            with psycopg2.connect(test_settings.db_dsn) as conn:
                 break
         except (psycopg2.DatabaseError, Exception):
-            print("Could not connect to postgres")
             time.sleep(1)
