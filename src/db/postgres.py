8cbe168b208e296c795dba47dca6c59a9b6b59c0
vladmeh <vladmeh@gmail.com>
2024-03-06 18:52:46 +0300

    Fix get session db.

--- src/db/postgres.py
+++ src/db/postgres.py
@@ -20,7 +20,13 @@ async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_c
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
-        yield session
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
 
 
 async def create_database() -> None:
